084b45825445b8dd228cd0536fca332365ef6249
Paweł Habrzyk <pawelhabrzyk@gmail.com>
2023-12-03 21:23:41 +0100

    model ulepiony z gliny

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import datetime
 from typing import List
-
 from fastapi import FastAPI, HTTPException, UploadFile, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -10,8 +9,22 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from dotenv import load_dotenv
 import joblib
-
+from kerastuner.tuners import RandomSearch
+from keras_tuner import HyperModel
+from fastapi import FastAPI, HTTPException, UploadFile, Depends
+from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+from fastapi.security import OAuth2PasswordRequestForm
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from dotenv import load_dotenv
+import joblib
+from keras.models import Sequential
+from keras.layers import Dense
 from db_manager import DatabaseManager
+from keras.models import load_model
+
 from jwt_utils import (
     create_access_token,
     create_refresh_token,
@@ -38,6 +51,19 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+model_name = "model"
+
+class MyHyperModel(HyperModel):
+    def __init__(self, input_shape):
+        self.input_shape = input_shape
+
+    def build(self, hp):
+        model = Sequential()
+        model.add(Dense(units=hp.Int('units', min_value=32, max_value=512, step=32),
+                        activation='relu', input_shape=self.input_shape))
+        model.add(Dense(1, activation='linear'))
+        model.compile(optimizer='adam', loss='mean_squared_error')
+        return model
 
 @app.post("/train")
 async def train_model(file: UploadFile, user: User = Depends(get_current_user)):
@@ -50,9 +76,32 @@ async def train_model(file: UploadFile, user: User = Depends(get_current_user)):
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
     )
-    model = LogisticRegression()
-    model.fit(X_train, y_train)
-    joblib.dump(model, "diabetes_model.joblib")
+    input_size = X_train.shape[1]  # Number of features
+
+    model = Sequential()
+    model.add(Dense(64, input_shape=(input_size,), activation='linear'))
+    model.add(Dense(32, activation='linear'))
+    model.add(Dense(16, activation='linear'))
+    model.add(Dense(1, activation='linear'))
+
+    model.compile(optimizer='adam', loss='mean_squared_error')
+
+    input_shape = [X_train.shape[1]]  # Assuming X_train is your input data
+    hypermodel = MyHyperModel(input_shape=input_shape)
+
+
+    tuner = RandomSearch(
+    hypermodel,
+    objective='val_loss',
+    max_trials=5,
+    executions_per_trial=3,
+    directory='my_dir',
+    project_name='hparam_tuning'
+    )
+
+    tuner.search(X_train, y_train, epochs=10, validation_data=(X_test, y_test))
+    best_model = tuner.get_best_models(num_models=1)[0]
+    best_model.save(model_name)
 
     return {"message": "Model trained and saved successfully"}
 
@@ -61,17 +110,17 @@ async def train_model(file: UploadFile, user: User = Depends(get_current_user)):
 async def predict_diabetes(
     input_data: PredictionInput, user: User = Depends(get_current_user)
 ):
+   
     try:
-        model = joblib.load("diabetes_model.joblib")
+        model = load_model(model_name)
     except FileNotFoundError:
         raise HTTPException(
             status_code=500, detail="Model not found. Please train the model first."
         )
-
-    input_df = pd.DataFrame([input_data.input.model_dump()])
+    i_data  = input_data.dict()['input']
+    input_df = pd.DataFrame([i_data])
     prediction = model.predict(input_df)
     prediction_native_type = int(prediction[0])  # or float, as appropriate
-
     if input_data.patient_id is not None:
         db.add_historical_data_to_patient(
             patient_id=input_data.patient_id,
@@ -90,6 +139,8 @@ async def predict_diabetes(
         )
 
     return {"prediction": prediction_native_type}
+    
+
 
 
 @app.get("/data", response_class=HTMLResponse)
